cb8bb5eb6b2e53941748d09846bae141853f4de6
Shailenderdazonn <115336729+Shailenderdazonn@users.noreply.github.com>
2023-04-19 16:50:33 +0530

    Add files via upload

---
+++ API/sell.php
@@ -0,0 +1,33 @@
+<?php
+include_once ("core.php");
+
+// Retrieve form data
+$title = $_POST["title"];
+$price = $_POST["price"];
+$description = $_POST["description"];
+$category = $_POST["category"];
+$location = $_POST["location"];
+$userid = $_POST["userid"];
+$file = $_FILES["file"];
+
+// Connect to the database
+$conn = mysqli_connect("localhost", "root", "", "barter_db");
+
+// Set the file path
+$filePath = "img/" . time() . "_" . $file["name"];
+
+// Move the uploaded file to the specified folder
+move_uploaded_file($file["tmp_name"], $filePath);
+$sql = "INSERT INTO user_ads (title, price, description, category, location, userid) VALUES ('$title', '$price', '$description', '$category', '$location', '$userid')";
+mysqli_query($conn, $sql); 
+
+// Update the MySQL table with the file path
+$sql = "UPDATE user_ads SET file='$filePath' WHERE id=LAST_INSERT_ID()";
+mysqli_query($conn, $sql);
+
+// Redirect to the desired URL
+header("Location: http://localhost:3000/products");
+exit; // Make sure to exit after the redirect    
+
+?>
+
